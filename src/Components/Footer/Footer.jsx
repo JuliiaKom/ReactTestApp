738042d68e0807c9fb619760e71642985aaa92a9
Yuliia <julkomisar@gmail.com>
2023-07-21 17:56:50 +0300

    remove import icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,5 @@
 import "./Footer.css";
-// import icon from "../../images/post.png";
-// import icon2 from "../../images/facebook.png";
-// import icon3 from "../../images/instagram.png";
+
 const Footer = (props) => {
     return (
         <footer>
